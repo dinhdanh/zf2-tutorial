827001717dd8c259cda97f5fbf15184a44d9da88
dinhdanh <dinhdanh.le@gmail.com>
2016-03-09 21:34:40 +0700

    re-aligntment

--- module/Album/src/Album/Form/AlbumForm.php
+++ module/Album/src/Album/Form/AlbumForm.php
@@ -7,8 +7,8 @@ class AlbumForm extends Form
 {
 	public function __construct($name = null)
 	{
-		parent::__construct('album');
-		$this->add(array(
+	    parent::__construct('album');
+	    $this->add(array(
             'name' => 'id',
             'type' => 'Hidden',
         ));
@@ -16,22 +16,22 @@ class AlbumForm extends Form
             'name' => 'title',
             'type' => 'Text',
             'options' => array(
-                'label' => 'Title',
+            'label' => 'Title',
             ),
         ));
         $this->add(array(
             'name' => 'artist',
             'type' => 'Text',
             'options' => array(
-                'label' => 'Artist',
+            'label' => 'Artist',
             ),
         ));
         $this->add(array(
             'name' => 'submit',
             'type' => 'Submit',
             'attributes' => array(
-                'value' => 'Go',
-                'id' => 'submitbutton',
+            'value' => 'Go',
+            'id' => 'submitbutton',
             ),
         ));
 	}
